0254751ac6e1b6a6da8852e65d06719a61bb2828
しゃけ <57042065+punipuni-3730@users.noreply.github.com>
2025-05-04 00:53:40 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const GoogleAppScriptURL = 'https://script.google.com/macros/s/AKfycbwx1DuCms5x7T6ROrupWJYO7UvtvtirCYGGj-_yCvarJNG_9VXjVmcx1cQoXJ6WPYdsQw/exec';
+const GoogleAppScriptURL = 'https://script.google.com/macros/s/AKfycbxUoOELR2Dk6K-NR4VcFDRQWCZPda_Ff6Clq8ixebfXL2Jv9A8VxqvDwWprw3LmAe4tyA/exec';
 
 document.addEventListener('DOMContentLoaded', function() {
   const usr = localStorage.getItem('username');
